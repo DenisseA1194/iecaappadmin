66d254839f4471bbb0576bb146ab6d85722b25e0
DenisseA1194 <74928019+DenisseA1194@users.noreply.github.com>
2024-01-16 15:37:55 -0600

    Initial Commit

---
+++ iecaappadmin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iecaappadmin
+//
+//  Created by Denisse Alejandra Martinez Mendiola on 16/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iecaappadmin/iecaappadminApp.swift
@@ -0,0 +1,17 @@
+//
+//  iecaappadminApp.swift
+//  iecaappadmin
+//
+//  Created by Denisse Alejandra Martinez Mendiola on 16/01/24.
+//
+
+import SwiftUI
+
+@main
+struct iecaappadminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
